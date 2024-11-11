c0375d212f85d2a8de5d7a0f82259f8efe40f01a
Ulifeliz <gasparelo@prof.educacao.sp.gov.br>
2024-11-11 15:55:45 -0300

    cartão de estudo
    cartão de estudo, pratica aulas do alura.

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Biologia',
+    'Quem foi Charles Darwin?',
+    'Darwin foi o primeiro a oferecer provas científicas e explicar o mecanismo que a torna possível: a seleção natural.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é o valor da constante de Planck?',
+    'Tem o valor aproximado de 6,626x10^-34 joules por segundo. Esta constante expressa, assim, a proporcionalidade entre a energia e a frequência de uma determinada radiação eletromagnética, possuindo dimensões de ação, e surge na maioria das leis relativas à física nuclear e às partículas elementares'
+)
+
+criaCartao(
+    'Matemática',
+    'Para que serve a equação de Euler?',
+    'A relação de Euler é usada para relacionar o número de faces, vértices e arestas de poliedros convexos. Assim, ela pode facilitar a contagem desses elementos. Ouça o texto abaixo! Nessa fórmula, V = número de vértices, A = número de arestas e F = número de faces.'
+)
+
+criaCartao(
+    'Química',
+    'Por que Lavoisier é considerado o pai da química moderna?',
+    'Considerado o pai da Química, Antoine Lavoisier foi o primeiro a observar que o oxigênio, em contato com uma substância inflamável, produz a combustão. Deduziu, também, baseado em reações químicas, a célebre lei da conservação da matéria: "Na natureza nada se cria, nada se perde, tudo se transforma".'
+)
+criaCartao(
+    'Astronomia',
+    'Quem é considerado o pai da Astronomia?',
+    'Nicolau Copérnico foi um importante matemático e astrônomo polonês. Pode ser chamado de “pai” da astronomia moderna, pois foi ele quem percebeu e defendeu a tese de que a Terra, assim como os demais planetas, gira em torno do sol, em uma teoria chamada de Heliocentrismo.'
+)    
